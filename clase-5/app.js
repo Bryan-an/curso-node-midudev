aa01ec43f48ccad5933864f36fb22980f640dd99
Bryan-an <bryanandagoya@gmail.com>
2023-09-16 19:02:04 -0500

    dependency injection

--- clase-5/app.js
+++ clase-5/app.js
@@ -1,32 +1,19 @@
 import express, { json } from 'express'; // require -> commonJS
-import { moviesRouter } from './routes/movies.js';
+import { createMovieRouter } from './routes/movies.js';
 import { corsMiddleware } from './middlewares/cors.js';
 
-// EN EL FUTURE: el import del json será así:
-// import movies from './movies.json' with {type: 'json'};
+export const createApp = ({ movieModel }) => {
+  const app = express();
 
-// como leer un json en ESModules
-// import fs from 'node:fs';
-// const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'));
+  app.use(json());
+  app.use(corsMiddleware());
+  app.disable('x-powered-by'); // deshabilitar el header X-Powered-By: Express
+  app.use('/movies', createMovieRouter({ movieModel }));
 
-// como leer un json en ESModules recomendado por ahora
+  /* eslint-disable no-undef */
+  const PORT = process.env.PORT || 1234;
 
-const app = express();
-app.use(json());
-app.use(corsMiddleware());
-app.disable('x-powered-by'); // deshabilitar el header X-Powered-By: Express
-
-// métodos normales: GET/HEAD/POST
-// métodos complejos: PUT/PATCH/DELETE
-
-// CORS PRE-FLIGHT
-// OPTIONS
-
-// Todos los recurrsos que sean MOVIES se identifican con /movies
-app.use('/movies', moviesRouter);
-
-const PORT = process.env.PORT || 1234;
-
-app.listen(PORT, () =>
-  console.log(`Server listening on port http://localhost:${PORT}`)
-);
+  app.listen(PORT, () =>
+    console.log(`Server listening on port http://localhost:${PORT}`)
+  );
+};
--- clase-5/controllers/movies.js
+++ clase-5/controllers/movies.js
@@ -1,24 +1,25 @@
-// import { MovieModel } from '../models/database/movie.js';
-// import { MovieModel } from '../models/local-file-system/movie.js';
-import { MovieModel } from '../models/mysql/movie.js';
 import { validateMovie, validatePartialMovie } from '../schemas/movies.js';
 
 export class MovieController {
-  static async getAll(req, res) {
+  constructor({ movieModel }) {
+    this.movieModel = movieModel;
+  }
+
+  getAll = async (req, res) => {
     const { genre } = req.query;
-    const movies = await MovieModel.getAll({ genre });
+    const movies = await this.movieModel.getAll({ genre });
     // qué es lo que renderiza
     res.json(movies);
-  }
+  };
 
-  static async getById(req, res) {
+  getById = async (req, res) => {
     const { id } = req.params;
-    const movie = await MovieModel.getById({ id });
+    const movie = await this.movieModel.getById({ id });
     if (movie) return res.json(movie);
     res.status(404).json({ message: 'Movie not found' });
-  }
+  };
 
-  static async create(req, res) {
+  create = async (req, res) => {
     const result = validateMovie(req.body);
 
     if (result.error) {
@@ -26,22 +27,22 @@ export class MovieController {
       return res.status(400).json({ error: JSON.parse(result.error.message) });
     }
 
-    const newMovie = await MovieModel.create(result.data);
+    const newMovie = await this.movieModel.create(result.data);
     res.status(201).json(newMovie); // actualizar la caché del cliente
-  }
+  };
 
-  static async delete(req, res) {
+  delete = async (req, res) => {
     const { id } = req.params;
-    const result = await MovieModel.delete({ id });
+    const result = await this.movieModel.delete({ id });
 
     if (result === false) {
       return res.status(404).json({ message: 'Movie not found' });
     }
 
     return res.json({ message: 'Movie deleted' });
-  }
+  };
 
-  static async update(req, res) {
+  update = async (req, res) => {
     const result = validatePartialMovie(req.body);
 
     if (!result.success) {
@@ -49,12 +50,15 @@ export class MovieController {
     }
 
     const { id } = req.params;
-    const updatedMovie = await MovieModel.update({ id, input: result.data });
+    const updatedMovie = await this.movieModel.update({
+      id,
+      input: result.data,
+    });
 
     if (updatedMovie === false) {
       return res.status(404).json({ message: 'Movie not found' });
     }
 
     return res.json(updatedMovie);
-  }
+  };
 }
